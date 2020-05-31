a083adc8827ca932d5b0e013d5035657a743345e
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-05-30 17:07:59 -0700

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -20,22 +20,10 @@ interface MagicClientSDKInterface {
   Magic: any;
 }
 
-// interface MagicPassportInterface {
-//   Strategy: any;
-// }
-
-// @deno-types="https://deno.land/x/types/react/v16.13.1/react.d.ts"
-// export { default as React } from "https://dev.jspm.io/react";
-// export { default as ReactDOMServer } from "https://dev.jspm.io/react-dom/server";
 import * as MagicClientSDK from "https://dev.jspm.io/magic-sdk";
 import * as MagicAdminSDK from "https://dev.jspm.io/@magic-sdk/admin";
-// import * as passportModule from "https://dev.jspm.io/passport";
-// import * as MagicPassport from "https://dev.jspm.io/passport-magic";
 
 export const { Magic: MagicClient } = MagicClientSDK
   .default as MagicClientSDKInterface;
 export const { Magic: MagicAdmin, SDKError } = MagicAdminSDK
   .default as MagicAdminSDKInterface;
-// export const passport = passportModule.default as Passport;
-// export const { Strategy: MagicStrategy } = MagicPassport
-//   .default as MagicPassportInterface;
